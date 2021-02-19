eab50e3436038f7161e012bcf7a440f9aa59b68a
deepakkarki <deepak6k@gmail.com>
2021-02-19 22:39:20 +0530

    more UI stuff, dropdown done

--- src/AllCourses.js
+++ src/AllCourses.js
@@ -72,19 +72,24 @@ function Nav(){
       <Menu.Item>
         Add new course
       </Menu.Item>
+      <Menu.Divider />
       <Menu.Item>
           2nd menu item
       </Menu.Item>
+      <Menu.Divider />
       <Menu.Item>
           3rd menu item
       </Menu.Item>
       <Menu.Item danger>a danger item</Menu.Item>
     </Menu>
   )
-
+  let history = useHistory()
+  const goToHome = (ev) => {
+    history.push(`/`)
+  }
   return (
     <nav className="all-courses-nav">
-      <Logo style={{height: "36px", padding:"3px"}}/>
+      <Logo style={{height: "36px", padding:"3px"}} onClick={goToHome}/>
       <p>COURSES</p>
       <Dropdown overlay={menu} trigger={['click']} placement="bottomRight">
         <PlusOutlined />
@@ -96,19 +101,49 @@ function Nav(){
 // the card ui for a single course
 function Course(props){
   const {name, code, vcount} = props
+  // const {onEdit, onArchive, onDelete} = props
+  const onEdit = () => console.log("edit")
+  const onArchive = () => console.log("archive")
+  const onDelete = () => console.log("delete")
 
   let history = useHistory()
-  const goToCourse = () => {
+  const goToCourse = (ev) => {
     history.push(`/notes/${code}`)
   }
+
+  const menu = courseCardMenu(onEdit, onArchive, onDelete)
+
   return(
     <div className="course-card" onClick={goToCourse}>
       <div className="course-card-header">
         <h2 className="course-code">{code}</h2>
-        <SettingFilled onClick={(e)=>{e.stopPropagation();console.log(e)}} />
+        <Dropdown 
+            overlay={menu}
+            trigger={['click']}
+            placement="bottomRight" 
+            overlayClassName="course-card-edit"
+        >
+          <SettingFilled onClick={e => e.stopPropagation()} />
+        </Dropdown>
       </div>
       <p  className="course-name">{name}</p>
       <p  className="course-vcount">{vcount} videos</p>
     </div>
   )
 }
+
+const courseCardMenu = (onEdit, onArchive, onDelete) => (
+  <Menu onClick={(e) => e.domEvent.stopPropagation()}>
+    <Menu.Item onClick={onEdit}>
+      Edit Course
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item onClick={onArchive}>
+      Archive Course
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item danger onClick={onDelete}>
+       Delete Course
+    </Menu.Item>
+  </Menu>
+)
